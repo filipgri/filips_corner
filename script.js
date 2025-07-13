ac931a11eed6904f1eecf7c34709d3e34e166ef8
filipgri <filip.bircanin@gmail.com>
2025-07-13 18:05:41 +0100

    apply

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 document.getElementById('navToggle').addEventListener('click', () => {
   document.getElementById('siteNav').classList.toggle('open');
 });
+
+// PROJECT-LEVEL toggles
+document.querySelectorAll('.project-toggle').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const project = btn.closest('.project');
+    const isOpen = project.classList.toggle('open');
+    btn.setAttribute('aria-expanded', isOpen);
+  });
+});
